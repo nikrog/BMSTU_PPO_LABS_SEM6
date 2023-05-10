edbe41caed4d769d3f4c07578e893b45f9ad4f6a
nikrog <arperinn@gmail.com>
2023-05-10 15:01:32 +0300

    Added full gui

---
+++ lab_07_2/src/business_logic/IRepositories/IRequestRepository.h
@@ -0,0 +1,21 @@
+#ifndef PPO_IREQUESTREPOSITORY_H
+#define PPO_IREQUESTREPOSITORY_H
+
+#include "../models/ModelRequest.h"
+#include "CRUDRepository.h"
+
+class IRequestRepository : public CRUDRepository<Request>{
+public:
+    virtual Request getRequestByID(int id) = 0;
+    virtual std::vector<Request> getRequestByState(State_t state) = 0;
+    virtual std::vector<Request> getRequestByDate(timereq_t date) = 0;
+    virtual std::vector<Request> getRequestByClient(int client_id) = 0;
+    virtual std::vector<Request> getRequestByManager(int manager_id) = 0;
+    virtual std::vector<Request> getRequestBySum(float min_sum, float max_sum) = 0;
+    virtual std::vector<Request> getRequestByDuration(int min_time, int max_time) = 0;
+    virtual std::vector<Request> getAllRequests() = 0;
+    virtual int addRequest(RequestInfo inf) = 0;
+};
+
+
+#endif //PPO_IREQUESTREPOSITORY_H
---
+++ lab_07_2/src/data_access/sql_scripts/fill_tables.sql
@@ -0,0 +1,36 @@
+INSERT INTO BA.users (login, password, permission) VALUES
+('admin', 'admin', 3);
+
+INSERT INTO BA.users (login, password, permission) VALUES
+('manager', '1111', 2);
+
+INSERT INTO BA.users (login, password, permission) VALUES
+('client', '2222', 1);
+
+INSERT INTO BA.users (login, password, permission) VALUES
+('client2', '2223', 1);
+
+INSERT INTO BA.banks (name, license_num, address, email, phone, website) VALUES
+('test2', 1234, 'testovaya ulitsa 2', 'test2@bank.ru', '+74953459872', 'testbank2.ru')
+RETURNING bank_id;
+
+INSERT INTO BA.banks (name, license_num, address, email, phone, website) VALUES
+('test3', 1234, 'testovaya ulitsa 3', 'test3@bank.ru', '+74953459873', 'testbank3.ru');
+
+INSERT INTO BA.clients (name, surname, patronymic, passport_num, birth_date, address, email, phone, user_id) VALUES
+('b', 'b', 'b', 77779, 1986, 'Street 9', 'b@mail.ru', '+79183456781', 4);
+
+INSERT INTO BA.products (ptype, name, bank_id, rate, min_time, max_time,
+                         min_sum, max_sum, currency, sum_rating, count_rating)  VALUES
+(0, 'test', 1, 7.5, 10, 750, 10000, 1000000, 0, 0, 0);
+
+INSERT INTO BA.products (ptype, name, bank_id, rate, min_time, max_time,
+                         min_sum, max_sum, currency, sum_rating, count_rating)  VALUES
+(1, 'test_credit', 1, 12.5, 1, 750, 100000, 3000000, 0, 21, 5);
+
+
+SELECT date FROM BA.requests
+WHERE date > '2023-05-06 15:20:55.000000';
+
+SELECT * FROM BA.banks
+ORDER BY bank_id DESC;
